c5236a286bb39dab8c65c811519c15dfe126d1d5
earlalay <earlpearlalayna@gmail.com>
2026-09-22 12:51:41 -0600

    Created some files to practice basic Javascript.

---
+++ ponder/week1/some_practice.js
@@ -0,0 +1,7 @@
+const my_apples = 4;
+const friend_apples = 2;
+let total = my_apples + friend_apples
+
+document.getElementById("my_apple_element").textContent = my_apples
+document.getElementById("friend_apple_element").textContent = friend_apples
+document.getElementById("total_apples_element").textContent = total;
